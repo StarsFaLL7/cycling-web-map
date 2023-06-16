9906151e5c14cc93a9c8604b24c4dd17754fd607
Vladislav Chistyakov <shirowayfy@gmail.com>
2023-06-16 18:14:45 +0500

    udt login form

--- frontend/components/Auth/Form.vue
+++ frontend/components/Auth/Form.vue
@@ -2,71 +2,31 @@
   <div class="auth-overflow" v-show="isOpen">
     <div class="auth-background" @click="emits('close-modal')"></div>
     <div
-      class="auth"
-      id="auth"
-      :class="{
+        class="auth"
+        id="auth"
+        :class="{
         'right-panel-active': selectedTab === 1,
       }"
     >
-      <div class="close" @click="emits('close-modal')">
-        <img src="~/assets/images/close.svg" alt="" />
-      </div>
-      <div class="form-container sign-up-container">
-        <form
-          action="#"
-          @submit.prevent="onRegister"
-          :class="{ error: registerError }"
-        >
-          <h1>Регистрация</h1>
-          <!-- <span>or use your email for registration</span> -->
-          <input type="text" placeholder="Имя" v-model="registerData.name" />
-          <input
-            type="email"
-            placeholder="Почта"
-            v-model="registerData.email"
-          />
-          <input
-            type="password"
-            placeholder="Пароль"
-            v-model="registerData.password"
-          />
-          <button>Зарегистрироваться</button>
-        </form>
-      </div>
-      <div class="form-container sign-in-container">
-        <form
-          action="#"
-          @submit.prevent="onLogin"
-          :class="{ error: loginError }"
-        >
-          <h1>Вход</h1>
-          <!-- <span>or use your account</span> -->
-          <input type="email" placeholder="Почта" v-model="loginData.email" />
-          <input
-            type="password"
-            placeholder="Пароль"
-            v-model="loginData.password"
-          />
-          <a href="#">Забыли пароль?</a>
-          <button>Войти</button>
-        </form>
-      </div>
-      <div class="overlay-container">
-        <div class="overlay">
-          <div class="overlay-panel overlay-left">
-            <h1>С Возвращением!</h1>
-            <p>Выполните вход с помощью ваших данных</p>
-            <button class="ghost" id="signIn" @click="selectedTab = 0">
-              Войти
-            </button>
-          </div>
-          <div class="overlay-panel overlay-right">
-            <h1>Добро Пожаловать!</h1>
-            <p>Введите свои данные и начните пользоваться нашим приложением</p>
-            <button class="ghost" id="signUp" @click="selectedTab = 1">
-              Зарегистрироваться
-            </button>
-          </div>
+      <div class="main">
+        <input type="checkbox" id="chk" aria-hidden="true">
+        <div class="signup">
+          <form @submit.prevent="onRegister">
+            <label for="chk" aria-hidden="true">Регистрация</label>
+            <input v-model="registerData.username" type="text" name="txt" placeholder="User name" required="">
+            <input v-model="registerData.email" type="email" name="email" placeholder="Email" required="">
+            <input v-model="registerData.password" type="password" name="pswd" placeholder="Password" required="">
+            <button>Sign up</button>
+          </form>
+        </div>
+
+        <div class="login">
+          <form @submit.prevent="onRegister">
+            <label for="chk" aria-hidden="true">Вход</label>
+            <input v-model="loginData.email" type="email" name="email" placeholder="Email" required="">
+            <input v-model="loginData.password" type="password" name="pswd" placeholder="Password" required="">
+            <button>Login</button>
+          </form>
         </div>
       </div>
     </div>
@@ -81,7 +41,7 @@ const selectedTab = ref(0);
 const emits = defineEmits(["close-modal"]);
 
 const registerData = ref({
-  name: "",
+  username: "",
   email: "",
   password: "",
 });
@@ -91,17 +51,17 @@ const loginData = ref({
   email: "",
 });
 
-const { login, register } = useStrapiAuth();
+const {login, register} = useStrapiAuth();
 
 const loginError = ref(false);
 const registerError = ref(false);
 
 const onLogin = async () => {
-  const { password, email: identifier } = loginData.value;
+  const {password, email: identifier} = loginData.value;
   loginError.value = false;
 
   try {
-    await login({ identifier, password });
+    await login({identifier, password});
 
     emits("close-modal");
   } catch (e) {
@@ -110,11 +70,11 @@ const onLogin = async () => {
 };
 
 const onRegister = async () => {
-  const { password, email, name } = registerData.value;
+  const {password, email, username} = registerData.value;
   registerError.value = false;
 
   try {
-    await register({ name, email, password, username: name });
+    await register({name: username, email, password, username});
     emits("close-modal");
   } catch (e) {
     console.log(e);
@@ -151,123 +111,213 @@ const onRegister = async () => {
   position: relative;
   background: white;
 
-  .close {
-    width: 15px;
-    height: 15px;
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    z-index: 1000;
-    opacity: 0.5;
-    cursor: pointer;
-    transition: all 0.2s;
-
-    &:hover {
-      opacity: 1;
-    }
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
+  //.close {
+  //  width: 15px;
+  //  height: 15px;
+  //  position: absolute;
+  //  top: 20px;
+  //  right: 20px;
+  //  z-index: 1000;
+  //  opacity: 0.5;
+  //  cursor: pointer;
+  //  transition: all 0.2s;
+  //
+  //  &:hover {
+  //    opacity: 1;
+  //  }
+  //
+  //  img {
+  //    width: 100%;
+  //    height: 100%;
+  //    object-fit: contain;
+  //    object-position: center;
+  //  }
+  //}
+  //
+  //h1 {
+  //  font-size: 28px;
+  //  font-weight: normal;
+  //  margin: 0;
+  //}
+  //
+  //h2 {
+  //  text-align: center;
+  //}
+  //
+  //p {
+  //  font-size: 14px;
+  //  font-weight: 100;
+  //  line-height: 20px;
+  //  letter-spacing: 0.5px;
+  //  margin: 20px 0 30px;
+  //}
+  //
+  //span {
+  //  font-size: 12px;
+  //}
+  //
+  //a {
+  //  color: #333;
+  //  font-size: 14px;
+  //  text-decoration: none;
+  //  margin: 15px 0;
+  //}
+  //
+  //button {
+  //  border-radius: 20px;
+  //  border: 1px solid $green-400;
+  //  background-color: $green-300;
+  //  color: #ffffff;
+  //  font-size: 12px;
+  //  font-weight: bold;
+  //  padding: 12px 45px;
+  //  letter-spacing: 1px;
+  //  text-transform: uppercase;
+  //  transition: transform 80ms ease-in;
+  //
+  //  cursor: pointer;
+  //
+  //  &:not(.ghost) {
+  //    margin-top: 10px;
+  //  }
+  //}
+  //
+  //button:active {
+  //  transform: scale(0.95);
+  //}
+  //
+  //button:focus {
+  //  outline: none;
+  //}
+  //
+  //button.ghost {
+  //  background-color: transparent;
+  //  border-color: #ffffff;
+  //}
+  //
+  //form {
+  //  background-color: #ffffff;
+  //  display: flex;
+  //  align-items: center;
+  //  justify-content: center;
+  //  flex-direction: column;
+  //  padding: 0 50px;
+  //  height: 100%;
+  //  text-align: center;
+  //
+  //  h1 {
+  //    margin-bottom: 10px;
+  //  }
+  //
+  //  &.error {
+  //    input {
+  //      border-color: red;
+  //    }
+  //  }
+  //}
+  //
+  //input {
+  //  background-color: #eee;
+  //  border: 1px solid #eee;
+  //  padding: 12px 15px;
+  //  margin: 8px 0;
+  //  width: 100%;
+  //  border-radius: 10px;
+  //
+  //  &:focus {
+  //    outline: none;
+  //  }
+  //}
+  .main {
+    width: 100%;
+    height: 500px;
+    background: red;
+    overflow: hidden;
+    background: $green-300;
+    border-radius: 10px;
+    box-shadow: 5px 20px 50px #000;
   }
 
-  h1 {
-    font-size: 28px;
-    font-weight: normal;
-    margin: 0;
+  #chk {
+    display: none;
   }
 
-  h2 {
-    text-align: center;
-  }
-
-  p {
-    font-size: 14px;
-    font-weight: 100;
-    line-height: 20px;
-    letter-spacing: 0.5px;
-    margin: 20px 0 30px;
-  }
-
-  span {
-    font-size: 12px;
-  }
-
-  a {
-    color: #333;
-    font-size: 14px;
-    text-decoration: none;
-    margin: 15px 0;
-  }
-
-  button {
-    border-radius: 20px;
-    border: 1px solid $green-400;
-    background-color: $green-300;
-    color: #ffffff;
-    font-size: 12px;
-    font-weight: bold;
-    padding: 12px 45px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    transition: transform 80ms ease-in;
-
-    cursor: pointer;
-
-    &:not(.ghost) {
-      margin-top: 10px;
-    }
-  }
-
-  button:active {
-    transform: scale(0.95);
-  }
-
-  button:focus {
-    outline: none;
-  }
-
-  button.ghost {
-    background-color: transparent;
-    border-color: #ffffff;
-  }
-
-  form {
-    background-color: #ffffff;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    padding: 0 50px;
+  .signup {
+    position: relative;
+    width: 100%;
     height: 100%;
-    text-align: center;
+  }
 
-    h1 {
-      margin-bottom: 10px;
-    }
-
-    &.error {
-      input {
-        border-color: red;
-      }
-    }
+  label {
+    color: #fff;
+    font-size: 2.3em;
+    justify-content: center;
+    display: flex;
+    margin: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: .5s ease-in-out;
   }
 
   input {
-    background-color: #eee;
-    border: 1px solid #eee;
-    padding: 12px 15px;
-    margin: 8px 0;
-    width: 100%;
-    border-radius: 10px;
-
-    &:focus {
-      outline: none;
-    }
+    width: 70%;
+    //height: 20px;
+    background: #e0dede;
+    justify-content: center;
+    display: flex;
+    margin: 20px auto;
+    padding: 10px;
+    border: none;
+    outline: none;
+    border-radius: 5px;
   }
+
+  button {
+    width: 70%;
+    height: 40px;
+    margin: 20px auto 10px;
+    justify-content: center;
+    display: block;
+    color: #fff;
+    background: $green-500;
+    font-size: 1em;
+    font-weight: bold;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    transition: .2s ease-in;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background: $green-600;
+  }
+
+  .login {
+    height: 460px;
+    background: #eee;
+    border-radius: 60% / 10%;
+    transform: translateY(-180px);
+    transition: .8s ease-in-out;
+  }
+
+  .login label {
+    color: $green-500;
+    transform: scale(.6);
+  }
+
+  #chk:checked ~ .login {
+    transform: translateY(-500px);
+  }
+
+  #chk:checked ~ .login label {
+    transform: scale(1);
+  }
+
+  #chk:checked ~ .signup label {
+    transform: scale(.6);
+  }
+
 }
 
 .auth {
@@ -275,8 +325,9 @@ const onRegister = async () => {
   border-radius: 10px;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   overflow: hidden;
-  width: 768px;
-  max-width: 100%;
+  //width: 768px;
+  width: 100%;
+  max-width: 400px;
   min-height: 480px;
 }
 
